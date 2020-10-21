0c03aafef97eebe23f4147a5ba36ce44cbd9357b
suchi-dev <suchitrasrinivas10@gmaial.com>
2020-10-21 17:13:41 +0530

    Added Post Mapping to submit form data

--- conference-mvc/src/main/java/com/sample/springmvc/controllers/RegistrationController.java
+++ conference-mvc/src/main/java/com/sample/springmvc/controllers/RegistrationController.java
@@ -3,16 +3,24 @@ package com.sample.springmvc.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.sample.springmvc.model.Registration;
 
 @Controller
 public class RegistrationController {
 
-
 	@GetMapping("registration")
 	public String getRegistration(@ModelAttribute("registration") Registration registration) {
-		System.out.println("Registered : "+registration.getName());
 		return "registration";
+
 	}
+
+	@PostMapping("registration")
+	public String addRegistration(@ModelAttribute("registration") Registration registration) {
+		System.out.println("Registration : " + registration.getName());
+		return "registration";
+
+	}
+
 }
