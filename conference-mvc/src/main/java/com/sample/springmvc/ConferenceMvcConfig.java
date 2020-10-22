c75160c8bb4670006cbe923e720b5cbfb3cbddd0
suchi-dev <suchitrasrinivas10@gmaial.com>
2020-10-22 12:35:43 +0530

    Added support for i18N

---
+++ conference-mvc/src/main/java/com/sample/springmvc/ConferenceMvcConfig.java
@@ -0,0 +1,61 @@
+package com.sample.springmvc;
+
+import java.util.Locale;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+public class ConferenceMvcConfig implements WebMvcConfigurer{
+
+	
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/files/**")
+		.addResourceLocations("/WEB-INF/pdf/");		
+		
+	}
+
+	
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(localeChangeInterceptor());
+		
+	}
+	@Bean
+	public LocaleResolver localeResolver() {
+		SessionLocaleResolver slr = new SessionLocaleResolver();
+		slr.setDefaultLocale(Locale.US);
+		return slr;
+	}
+	
+	
+	@Bean
+	public LocaleChangeInterceptor localeChangeInterceptor() {
+		LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
+		lci.setParamName("lang");
+		return lci;
+	}
+	
+	@Bean
+	public ViewResolver viewResolver() {
+		InternalResourceViewResolver ivr = new InternalResourceViewResolver();
+		ivr.setPrefix("/WEB-INF/jsp/");
+		ivr.setSuffix(".jsp");
+		ivr.setOrder(0);
+		return ivr;
+		
+	}
+	
+	
+	
+	
+}
--- conference-mvc/src/main/webapp/WEB-INF/jsp/registration.jsp
+++ conference-mvc/src/main/webapp/WEB-INF/jsp/registration.jsp
@@ -1,6 +1,7 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
- <%@taglib prefix="form" uri="http://www.springframework.org/tags/form"  %>   
+<%@taglib prefix="form" uri="http://www.springframework.org/tags/form"  %>   
+<%@taglib prefix="spring" uri="http://www.springframework.org/tags" %>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
@@ -8,17 +9,17 @@
 <title>Registration Page</title>
 </head>
 <body>
-<h1>Registration</h1>
+<h1><spring:message code="registration"/></h1>
 <form:form modelAttribute="registration">
 <table>
 	<tr>
-	<td>Name:</td>
+	<td><spring:message code ="name"/></td>
 	<td><form:input path="name"/>
 	</td>
 	</tr>
 	<tr>
 	<td colspan="2">
-	<input type ="submit" value="Add Registration"/>
+	<input type ="submit" value="<spring:message code="save.changes"/>"/>
 	</td>
 	</tr>
 </table>
